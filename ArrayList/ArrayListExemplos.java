56bdd122e00efad387a77dea5a8b833eea1a6ce2
roger437unix <33252885+roger437unix@users.noreply.github.com>
2020-03-03 10:31:14 -0300

    Add files via upload

---
+++ ArrayList/ArrayListExemplos.java
@@ -0,0 +1,121 @@
+﻿
+// Exemplos de utilização de ArrayList *
+
+import java.util.ArrayList;
+
+public class ArrayListExemplos {
+	
+	public static void main(String[] args) {
+		
+		//****** Declarando ArrayList
+				
+				ArrayList<Integer> lista1 = new ArrayList<Integer>();
+				ArrayList<Double>  lista2 = new ArrayList<>();
+				ArrayList<String>  lista3 = new ArrayList<>();
+				
+
+		//***** Adiciando itens à lista
+				
+				lista1.add(1);
+				lista1.add(-9);
+				lista1.add(3);		
+				
+				lista2.add(3.14);
+				lista2.add(1.618);
+				lista2.add(2.71);
+				
+				lista3.add("Pitágoras");
+				lista3.add("Fibonacci");
+				lista3.add("Euler");
+				
+				
+		//****** Adicionar elemento em uma determinada posição da lista
+				
+				lista3.add(1, "Galileu");
+								
+
+		//****** Mostrando todos elementos da lista
+				
+				System.out.println();
+				for (int i=0 ; i<lista1.size() ; i++) {
+					System.out.println(lista1.get(i));
+				}
+				
+				System.out.println();
+				
+				for (String elemento: lista3) {
+					System.out.println(elemento);			
+				}
+						
+				System.out.println();
+				
+		
+		//****** Alterando um elemento da lista
+				
+				lista3.set(3, "Leibniz");
+				
+				for (String elemento: lista3) {
+					System.out.println(elemento);			
+				}
+				System.out.println();
+				
+				
+		//****** Obtendo o índice de um elemento da lista
+				
+				lista3.indexOf("Fibonacci");				
+				
+		
+		//****** Removendo um elemento da lista
+				
+				lista3.remove("Galileu");
+				
+					
+		// ***** Limpando toda a lista
+				
+				lista3.clear();
+				
+				
+		//***** Tamanho da Lista
+				
+				//System.out.print("\nTamanho da lista: ");
+				//System.out.println(lista1.size());
+				
+				
+		//***** ArrayList em Array [vetor]
+				
+				System.out.println(lista3.toArray());
+				
+				for (int i=0 ; i<lista3.toArray().length ; i++) {
+					System.out.println(lista3.toArray()[i]);
+				}
+				
+				
+		//***** Criando String com os elementos do ArrayList		
+				
+				String str2 = lista3.toString();
+				//System.out.println(str2);
+				
+				for (int i=0 ; i<str2.length() ; i++) {
+					//System.out.println(str2.charAt(i));
+				}				
+				
+
+		//****** ArrayList com elementos de tipos diferentes
+				
+						ArrayList<Object> lista = new ArrayList<>();
+						
+				        lista.add(10);
+				        lista.add(4.78);
+				        lista.add("Pink Floyd");
+				        lista.add('\u2588');
+				        lista.add(true);
+				        
+				        System.out.println();
+				        System.out.println(lista.get(0));
+				        System.out.println(lista.get(1));
+				        System.out.println(lista.get(2));
+				        System.out.println(lista.get(3));
+				        System.out.println(lista.get(4));		
+			}
+
+}
---
+++ ArrayList/QuantidadeNaoPrimoLista.java
@@ -0,0 +1,65 @@
+﻿
+
+// Informa todos os número que são divisíveis por uma número não primo *
+// Usa ArrayList
+// Usa tratamento de exceção
+
+import java.util.ArrayList;
+import java.util.Scanner;
+import java.util.InputMismatchException;
+
+public class QuantidadeNaoPrimoLista {
+	
+	public static void main(String[] args) {		
+		Scanner ler = new Scanner(System.in);
+		System.out.println();
+		long numero = 2L;	
+		System.out.println();
+		try {
+			while (true) {
+				System.out.print("Informe um inteiro positivo >=2 : ");
+				numero = ler.nextLong();
+				if (numero >= 2) {
+					break;
+				}				
+			}						
+		}		
+		catch(InputMismatchException erro){  
+			System.out.println("\nOops, isso não é um número inteiro!!!\n");	
+			System.exit(0);
+        }		
+		catch (Exception erro) {
+			System.out.println("\nErro indefinido\n");		
+			System.exit(0);
+		}
+		primo(numero);
+		ler.close();					
+	}
+	
+	
+	public static void primo(long num) {	
+		
+		ArrayList <Long> lista = new ArrayList<Long>();		
+		boolean ref = true;
+		
+		for (long i=2L ; i<=num/2 ; i++) {
+			if (num % i == 0) {
+				if (ref) {
+					System.out.println("\n" + num + " não é primo pois é divisível por:\n");
+					ref = !ref;					
+				}
+				lista.add(i);				
+			}
+		}
+		
+		if (ref) {
+			System.out.println("\n" + num + " é primo.\n");			
+		}	
+		else {
+			for (int i=0 ; i< lista.size() ; i++) {
+				System.out.print(lista.get(i) + "  ");
+			}			
+		}
+	}
+	
+}
